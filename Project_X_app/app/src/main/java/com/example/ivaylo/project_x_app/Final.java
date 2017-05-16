61177b5ba4b5a46c6f72b789567a2bc33205f3b9
IvayloVelkov <ivchovelkov@gmail.com>
2017-05-17 00:08:55 +0300

    The begging of judging

--- Project_X_app/app/src/main/java/com/example/ivaylo/project_x_app/Final.java
+++ Project_X_app/app/src/main/java/com/example/ivaylo/project_x_app/Final.java
@@ -1,51 +1,62 @@
 package com.example.ivaylo.project_x_app;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
-
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-public class Final extends AppCompatActivity  {
-    private Button sendBTN;
+public class Final extends Activity {
+    Button sendBTN;
+    EditText result;
+    Bundle extras = new Bundle();
+    String value = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_final);
+        sendBTN = (Button) findViewById(R.id.sendBTN);
         setButton();
+        extras = getIntent().getExtras();
+        value = extras.getString("Result");
     }
 
     private void setButton() {
         sendBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                result.setText(value);
                 Thread t =  new Thread() {
                     @Override
                     public void run() {
                         try {
                             System.out.println("Starting Connection");
-                            Socket s = new Socket("192.168.1.103", 3306);
+                            Socket s = new Socket("192.168.1.105", 3306);
                             System.out.println("Connection DONE");
                             DataOutputStream dos = new DataOutputStream(s.getOutputStream());
-                            dos.writeUTF(".");
+                            dos.writeUTF(String.valueOf(result));
                             dos.flush();
                             dos.close();
                             s.close();
                             System.out.println("Closing socket");
                         } catch (UnknownHostException e) {
-                            System.out.println("There was an Unknown Erorr:");
+                            Log.e(" etgr", "There was an Unknown Erorr:");
                             e.printStackTrace();
                         } catch (IOException e) {
                             System.out.println("There was an IOException:");
                             e.printStackTrace();
+                        } catch (Exception e) {
+                            Log.e("err", e.toString());
                         }
                     }
                 };
--- Project_X_app/app/src/main/java/com/example/ivaylo/project_x_app/TV_judging.java
+++ Project_X_app/app/src/main/java/com/example/ivaylo/project_x_app/TV_judging.java
@@ -1,26 +1,53 @@
 package com.example.ivaylo.project_x_app;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Checkable;
 
-public class TV_judging extends AppCompatActivity {
+public class TV_judging extends Activity {
+
+    Button readyBTN;
+    CheckBox A_check;
+    CheckBox B_check;
+    double result;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tv_judging);
 
-        Button readyBTN = (Button) findViewById(R.id.readyBTN);
+         readyBTN = (Button) findViewById(R.id.readyBTN);
+         A_check = (CheckBox) findViewById(R.id.A_balance);
+         B_check = (CheckBox) findViewById(R.id.B_balance);
+
+        if(A_check.isChecked()){
+            A_check.setChecked(true);
+            result = 0.10;
+        }
+        else if(B_check.isChecked()){
+            B_check.setChecked(true);
+            result = 0.20;
+        }
+        else {
+            result = 0;
+        }
 
         readyBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent startFinal = new Intent(getApplicationContext(),Final.class);
+                startFinal.putExtra("Result", result);
                 startActivity(startFinal);
             }
         });
+
     }
+
+
 }
